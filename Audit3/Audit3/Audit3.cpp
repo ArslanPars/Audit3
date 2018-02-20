9acdcad4c859697575f2856b4dcf8aff80b3a049
ArslanPars <35731432+ArslanPars@users.noreply.github.com>
2018-02-20 14:59:36 +0200

    Add files via upload

---
+++ Audit3/Audit3/Audit3.cpp
@@ -0,0 +1,37 @@
+// Audit3.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define COLS 11
+int main()
+{
+	int m[][COLS] = {
+		{ 0,0,0,0,1,1,1,0,0,0 },
+		{ 0,0,0,1,0,0,0,1,0,0 },
+		{ 0,0,1,0,0,0,0,0,1,0 },
+		{ 0,1,0,0,1,0,1,0,0,1 },
+		{ 1,0,0,0,0,0,0,0,0,0,1 },
+		{ 1,0,0,0,0,0,0,0,0,0,1 },
+		{ 1,0,0,1,0,0,0,1,0,0,1 },
+		{ 0,1,0,0,1,1,1,0,0,1 },
+		{ 0,0,1,0,0,0,0,0,1,0 },
+		{ 0,0,0,1,0,0,0,1,0,0 },
+		{ 0,0,0,0,1,1,1,0,0,0 },
+	};
+
+
+	int *pi = *m;
+	int i, mn = sizeof(m) / sizeof(int);
+	for (i = 0; i < mn; ++i, ++pi)
+	{
+		printf("%c%c", *pi, (i + 1) % COLS ? '\0' : '\n');
+	}
+
+	return 0;
+}
+
